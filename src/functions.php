75dbb13cc7d6e49e486215d40f201e9f41f70a9a
ruvarvel <ruvave@alumnatflorida.es>
2024-09-27 10:52:08 +0200

    Resolviendo conflictos en functions.php

--- src/functions.php
+++ src/functions.php
@@ -6,9 +6,9 @@ function mensajepersonalizado(){
     echo "Hola mensaje personalizado";
 }
 mensajepersonalizado();
-<<<<<<< HEAD
+
 //creando conflicto en la rama main y funcionalidad-1
-=======
+
 //creando conflicto en la rama main y funcionalidad-1
->>>>>>> funcionalidad-1
+
 ?>
